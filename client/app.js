f30d092f15243396df6d8caf4c96e6fc495e5c83
Sean B <sean@volcashare.com>
2020-03-28 14:31:44 +0100

    Install jest for testing (and more.  See below.)
    This change does a few things:
     - Install jest so I can start writing tests.
     - Use the module version of matter-js, poly-decomp, and p5 instead of
       using the client scripts.  This was done so the main plinko.js file
       could require them when testing on the server side.
     - Use browserify to compile backend javascripts for use in the browser.
     - Add watchify and nodeman to watch for file changes and restart the
       node server on those changes.
    
    NOTE: The reason I'm taking a snapshot here, without an actual test that
    tests the plinko logic, is because I'm having a hard time accessing
    individual methods in the main 'App' object.  I assume this is because
    the wy I wrote the main plinko script may be unconventional and
    therefore harder to use in a testing context.

---
+++ client/app.js
@@ -0,0 +1,5 @@
+
+const Matter = require('./matter.min.js');
+const p5 = require('./p5.min.js');
+const plinko = require('./plinko.js')
+
---
+++ test/plinko.test.js
@@ -0,0 +1,5 @@
+const plinko = require('../public/app');
+
+test('can be tested', () => {
+  expect(1 + 1).toBe(2);
+});
